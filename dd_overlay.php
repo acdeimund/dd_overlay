cadebcb06beeea780da2e35a1f61030284640811
Aaron Deimund <aaron@deimunddesigns.com>
2019-05-01 04:21:24 -0500

    getting the overlay to behave

--- dd_overlay.php
+++ dd_overlay.php
@@ -1,24 +1,24 @@
 <?php
 /**
- * Plugin Name: dd_overlay
- * Plugin URI: https://github.com/acdeimund/dd_overlay
- * Description: A WordPress plug-in for adding an overlay picture to the home page.
+ * Plugin Name: dd_secondary_logo
+ * Plugin URI: https://github.com/acdeimund/dd_secondary_logo
+ * Description: A WordPress plug-in for adding a secondary logo.
  * Version: 1.0
  * Author: Aaron Deimund
  * Author URI: https://www.deimunddesigns.net
  */
 
-function overlay_post_type() {
+function secondary_logo_post_type() {
     $labels = array(
-        "name" => "Overlay",
-        "singular_name" => "Overlay",
-        "add_new" => "Add Overlay",
-        "all_items" => "All Overlays",
-        "add_new_item" => "Add Overlay",
-        "edit_item" => "Edit Overlay",
-        "new_item" => "New Overlay",
-        "view_item" => "View Overlay",
-        "search_item" => "Search Overlays",
+        "name" => "Secondary Logo",
+        "singular_name" => "Secondary Logo",
+        "add_new" => "Add Secondary Logo",
+        "all_items" => "All Secondary Logos",
+        "add_new_item" => "Add Secondary Logo",
+        "edit_item" => "Edit Secondary Logo",
+        "new_item" => "New Secondary Logo",
+        "view_item" => "View Secondary Logo",
+        "search_item" => "Search Secondary Logos",
         "not_found" => "Sorry, nothing found",
         "not_found_in_trash" => "Sorry, nothing found in trash",
         "parent_item_colon" => "Parent Section"
@@ -42,30 +42,32 @@ function overlay_post_type() {
         "exclude_from_search" => false,
         "menu_icon" => "dashicons-format-gallery"
     );
-    register_post_type("overlay", $args);
+    register_post_type("secondary_logo", $args);
 }
 
-add_action("init", "overlay_post_type");
+add_action("init", "secondary_logo_post_type");
 
 // Add style and scripts for the plug-in
-function dd_overlay_scripts() {
+function dd_secondary_logo_scripts() {
 
   // // Plug-in stylesheet
-  wp_enqueue_style( 'dd_overlay_style', plugins_url( '/style.css', __FILE__ ) );
+  wp_enqueue_style( 'dd_secondary_logo_style', plugins_url( '/style.css', __FILE__ ) );
 
   // // Plug-in scripts
-  wp_enqueue_script( 'dd_overlay_scripts', plugins_url( '/scripts.js', __FILE__ ) );
+  wp_enqueue_script( 'dd_secondary_logo_scripts', plugins_url( '/scripts.js', __FILE__ ) );
 }
 
-add_action( 'wp_enqueue_scripts', 'dd_overlay_scripts' );
+add_action( 'wp_enqueue_scripts', 'dd_secondary_logo_scripts' );
 
-function dd_add_overlay(){
+function dd_add_secondary_logo(){
   global $post;
 
-  $args = array( "posts_per_page" => 1,"post_type" => "overlay" );
-  $overlay = get_posts( $args );
-  foreach ( $overlay as $post ) : setup_postdata( $post ); ?>
-    <img class = "dd_overlay <?php the_title(); ?>" src = "<?php the_post_thumbnail_url(); ?>);">
+  $args = array( "posts_per_page" => 1,"post_type" => "secondary_logo" );
+  $secondary_logo = get_posts( $args );
+  foreach ( $secondary_logo as $post ) : setup_postdata( $post ); ?>
+    <a href="<?php echo get_site_url(); ?>">
+      <img class = "dd_secondary_logo <?php the_title(); ?>" src = "<?php the_post_thumbnail_url(); ?>);">
+    </a>
   <?php endforeach; 
   wp_reset_postdata();
 }
